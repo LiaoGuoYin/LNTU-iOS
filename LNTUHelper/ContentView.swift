4d99e275a41c0ceb56c7afc94c7e68036ae07203
LiaoGuoYin <liaoguoyin@live.com>
2020-10-25 15:02:02 +0800

    Initial Commit

---
+++ LNTUHelper/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LNTUHelper
+//
+//  Created by LiaoGuoYin on 2020/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
