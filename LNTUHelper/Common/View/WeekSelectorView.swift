3ef20717e5b5f7c39b9c71e18f8247aab7fa8027
Wilson Yu <5325955+Xkinler@users.noreply.github.com>
2021-01-03 11:27:29 +0800

    [UI Update] show week options in WeekSelectorView in grids (without using iOS 14's implementation)

--- LNTUHelper/Common/View/WeekSelectorView.swift
+++ LNTUHelper/Common/View/WeekSelectorView.swift
@@ -18,24 +18,28 @@ struct WeekSelectorView: View {
             Text(title)
                 .foregroundColor(Color("primary"))
                 .frame(width: title == "" ? 0 : 63)
-            
-            ScrollView(.horizontal, showsIndicators: false) {
-                HStack(spacing: 2) {
-                    ForEach(1...numberList.count, id: \.self) { index in
-                        Text(String(numberList[index - 1]))
-                            .font(.caption)
-                            .foregroundColor(.white)
-                            .padding(11)
-                            .frame(width: 38, height: 30)
-                            .background((selectedIndex == index) ? Color("primary") : Color("primary").opacity(0.4))
-                            .cornerRadius(3.0)
-                            .onTapGesture(perform: {
-                                Haptic.shared.tappedHaptic()
-                                self.selectedIndex = index
-                            })
-                    }
+          VStack(alignment: .leading) {
+            let rowNumber = (numberList.count % 7 == 0 ? numberList.count / 7 : numberList.count / 7 + 1)
+            ForEach(1...rowNumber, id:\.self) { row in
+              HStack(spacing: 2) {
+                let startNumber = (row - 1) * 7 + 1
+                let endNumber = (row * 7 <= numberList.count ? row * 7 : numberList.count)
+                ForEach(startNumber...endNumber, id: \.self) { index in
+                    Text(String(numberList[index - 1]))
+                        .font(.caption)
+                        .foregroundColor(.white)
+                        .padding(11)
+                        .frame(width: 38, height: 30)
+                        .background((selectedIndex == index) ? Color("primary") : Color("primary").opacity(0.4))
+                        .cornerRadius(3.0)
+                        .onTapGesture(perform: {
+                            Haptic.shared.tappedHaptic()
+                            self.selectedIndex = index
+                        })
                 }
+              }
             }
+          }
         }
     }
 }
@@ -43,7 +47,8 @@ struct WeekSelectorView: View {
 
 struct WeekSelectorView_Previews: PreviewProvider {
     static var previews: some View {
-        let weekList = ["一", "二", "三", "四", "五", "六", "日"]
-        WeekSelectorView(title: .constant("星期天"), selectedIndex: .constant(2), numberList: weekList)
+//        let weekList = ["一", "二", "三", "四", "五", "六", "日"]
+      let longWeekList = Array(1...22).map { String($0) }
+        WeekSelectorView(title: .constant("第周"), selectedIndex: .constant(10), numberList: longWeekList)
     }
 }
