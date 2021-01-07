4f1e29099d21580fed6d1b3c7602c039aadf82b1
LiaoGuoYin <liaoguoyin@live.com>
2021-01-07 13:53:53 +0800

    [UI Update] Replaced the Toggle of GradeView with SegmentPicker for intuition

--- LNTUHelper/Screens/GradeAndQualityActivity/GradeAndQualityActivityView.swift
+++ LNTUHelper/Screens/GradeAndQualityActivity/GradeAndQualityActivityView.swift
@@ -10,49 +10,59 @@ import SwiftUI
 struct GradeAndQualityActivityView: View {
     
     @State var viewModel: GradeViewModel
-    @State private var isShowQuality: Bool = false
     @State private var qualityViewModel = QualityActivityViewModel()
     @ObservedObject var router = ViewRouter.router
+    @State private var selectingGradeMode = GradeMode.education
     
     var body: some View {
         NavigationView {
-            if isShowQuality {
+            if selectingGradeMode == GradeMode.education {
+                GradeView(viewModel: viewModel, tappedCourse: MockData.gradeList.first!)
+                    .navigationBarItems(leading: refreshButton, trailing: selectingGradePicker)
+                    .onAppear(perform: {
+                        Haptic.shared.tappedHaptic()
+                        if !Constants.isLogin { router.isShowLoginView = true }
+                    })
+            } else  {
                 QualityActivityView(viewModel: qualityViewModel)
                     .onAppear(perform: {
                         qualityViewModel.refreshToGetActivityList()
                     })
-            } else {
-                GradeView(viewModel: viewModel, tappedCourse: MockData.gradeList.first!)
-                    .navigationBarItems(leading: refreshButton, trailing: modeSwitchButton)
             }
         }
-        .onAppear(perform: {
-            Haptic.shared.tappedHaptic()
-            if !Constants.isLogin { router.isShowLoginView = true }
-        })
     }
     
-    var modeSwitchButton: some View {
-        Toggle(isOn: $isShowQuality) {
-            Text("素拓模式")
+    var selectingGradePicker: some View {
+        HStack {
+            Picker(selection: $selectingGradeMode, label: Text("成绩模式")) {
+                ForEach(GradeMode.allCases) {
+                    Text($0.rawValue).tag($0)
+                }
+            }
+            .pickerStyle(SegmentedPickerStyle())
         }
     }
     
     var refreshButton: some View {
         Button(action: {
             Haptic.shared.tappedHaptic()
-            self.refresh()
+            viewModel.refreshGradeList { (isSuccess) in
+                router.isShowLoginView = isSuccess ? false : true
+                router.banner = viewModel.banner
+            }
         }) {
             Text("刷新")
                 .foregroundColor(Color("primary"))
         }
     }
+}
+
+enum GradeMode: String, CaseIterable, Identifiable {
+    case quality = "素拓记录"
+    case education = "教务成绩"
     
-    func refresh() {
-        viewModel.refreshGradeList { (isSuccess) in
-            router.isShowLoginView = isSuccess ? false : true
-            router.banner = viewModel.banner
-        }
+    var id: String {
+        return self.rawValue
     }
 }
 
